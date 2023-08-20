c991e483b7e5c68ae61ebdd610a30f952b918676
marcs-feh <marcosfehlauer@protonmail.com>
2023-08-20 17:25:03 -0300

    add grayscale loader

---
+++ load_grayscale.py
@@ -0,0 +1,35 @@
+# Load TIFF File as a normalized grayscale matrix
+
+import cv2 as cv
+import numpy as np
+
+def load_normalized_grayscale(path: str, depth: int = 8) -> np.ndarray:
+  img = cv.imread(path, cv.IMREAD_GRAYSCALE)
+  N = np.float32((2 ** depth) - 1)
+  def normalize(n: np.float32):
+    return n / N
+
+  normalizev = np.vectorize(normalize)
+
+  norm = img.astype(np.float32)
+  norm = normalizev(norm)
+
+  return norm
+
+def write_normalized_grayscale(img: np.ndarray, outfile: str, depth: int = 8):
+  N = np.float32((2 ** depth) - 1)
+
+  def denormalize(n: np.float32):
+    return np.floor(n * N)
+
+  denormalizev = np.vectorize(denormalize)
+  denorm = denormalizev(img).astype(np.uint8) # TODO: This ignores depth, add an if-else or something
+
+  cv.imwrite(outfile, denorm)
+
+
+def main():
+  img = load_normalized_grayscale('picture.tiff')
+  write_normalized_grayscale(img, 'picture-clone.tiff')
+
+if __name__ == '__main__': main()
