6493b7bca4bd6adca2d296b31049739bf7db7257
marcs-feh <marcosfehlauer@protonmail.com>
2023-09-02 18:37:14 -0300

    add convolution

--- convolution.py
+++ convolution.py
@@ -1,16 +1,33 @@
 import numpy as np
+import cv2 as cv
 
 def valid_mask_shape(mask: np.ndarray) -> bool:
   is_odd = lambda x: (x % 2) != 0
-  return ((mask.shape[0] == mask.shape[1]) and
-           is_odd(mask.shape[0]) and is_odd(mask.shape[1]))
+  return (mask.shape[0] == mask.shape[1]) and is_odd(mask.shape[0])
+
+
+def mat_bounds_check(row, col, height, width) -> bool:
+  ok = (col < width) and (col > 0) and (row < height) and (row > 0)
+  return ok
+
 
 def convolution(img: np.ndarray, mask: np.ndarray) -> np.ndarray:
-  ok = (mask.shape[0] < img.shape[0]) and (mask.shape[1] < img.shape[1]) and valid_mask_shape(mask)
-  assert ok
+  assert valid_mask_shape(mask)
 
-  
+  N = mask.shape[0] // 2
+  border_img = cv.copyMakeBorder(img, N,N,N,N, cv.BORDER_CONSTANT, value=(0,0,0))
+  res = np.zeros( img.shape )
+  HEIGHT, WIDTH = border_img.shape
 
-  res = img.copy()
+  # print(f'Border w:{WIDTH} h:{HEIGHT}')
+  # print(f'Original w:{img.shape[0]} h:{img.shape[1]}')
+  for row in range(N, HEIGHT - N):
+    for col in range(N, WIDTH - N):
+      slice = border_img[(row-N):(row+N+1), (col-N):(col+N+1)]
+      if slice.shape != (3,3):
+        print(f'({row}, {col}) -> {slice}')
+      total = np.sum(mask * slice)
+      res[row - N, col - N] = total
+
 
   return res
--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from blending_ops import *
 from convolution import *
 
 def main():
-  img = load_normalized_grayscale('camera.tiff')
-  noise = np.random.normal(0, 0.25, img.shape)
+  img = load_normalized_grayscale('touhou_weed.tiff')
+  # noise = np.random.normal(0, 0.25, img.shape)
 
   # res = add_img(img, noise)
   # write_normalized_grayscale(res, 'add.out.tiff')
@@ -12,13 +12,19 @@ def main():
   # res = mul_img(img, noise)
   # write_normalized_grayscale(res, 'mul.out.tiff')
 
-  conv_mask = np.array([
-    [0.1, 0.1, 0.8],
-    [0.2, 0.8, 0.1],
-    [0.1, 0.1, 0.3],
-  ])
+  horizontal = np.array([
+    [-1, 0, 1],
+    [-2, 0, 2],
+    [-1, 0, 1]]) * 0.25
 
-  res = convolution(img, conv_mask)
+  vertical = np.array([
+    [-1, -2, -1],
+    [0, 0, 0],
+    [1, 2, 1]]) * 0.25
+
+  res_h = convolution(img, horizontal)
+  res_v = convolution(img, vertical)
+  res = add_img(res_h, res_v)
   write_normalized_grayscale(res, 'conv.out.tiff')
 
 if __name__ == '__main__': main()
